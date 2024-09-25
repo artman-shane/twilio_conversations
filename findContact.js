1d9111f54c6aea131a18bd68d5621252c120d242
sartman <sartman@twilio.com>
2024-09-25 17:51:35 -0400

    Output only the conversation SID and message Bodies

--- findContact.js
+++ findContact.js
@@ -5,10 +5,12 @@ dotenv.config();
 
 const manageConversations = new ManageConversations();
 
-const conversations = await manageConversations.findConversationsByContact(process.env.CONTACT_LOOKUP_NUMBER);
+const conversations = await manageConversations.findConversationsByContact(
+  process.env.CONTACT_LOOKUP_NUMBER
+);
 console.log(`Here are some conversations:`);
 const messages = await conversations.forEach((conversation) => {
-  console.log(conversation.conversationSid);
+  // console.log(conversation.conversationSid);
   const _messages = manageConversations
     .getMessageBody(conversation.conversationSid)
     .then((messages) => {
